07cc00a69a36a70c35200dd463ceb3a70e1ec85a
yuttei1234 <chukyo03210321@yahoo.co.jp>
2021-08-01 10:01:39 +0000

    [Finish]3-1~7

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
+  
+end
