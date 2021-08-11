2784ebb46912e8768b990cade1d3c3494ea99dbd
yuttei1234 <chukyo03210321@yahoo.co.jp>
2021-08-11 01:54:23 +0000

    [Finish] 3-7 Revised

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,25 +1,20 @@
 class BlogsController < ApplicationController
   def index
+    @blogs=Blog.all
   end
 
   def show
     @blog = Blog.find(params[:id])
   end
 
-  def index
-    @blogs = Blog.all
-
-  end
-
   def new
     @blog = Blog.new
-
   end
 
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(blog.id)
+    redirect_to blogs_path(blog.id)
   end
 
 
@@ -43,6 +38,4 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-
-
 end
